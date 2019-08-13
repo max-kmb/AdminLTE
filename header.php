3e2b4d18bdebb0919bc1ddf42a02e853301dc1c4
Maksim Kubrak <web@maksimkubrak.ru>
2019-08-13 16:38:27 +0300

    remove search from menu

--- header.php
+++ header.php
@@ -57,19 +57,6 @@ $arLangMessage = \Bitrix\Main\Localization\Loc::loadLanguageFile(__FILE__, strto
       ),
       false
     );?>
-
-    <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
-      <div class="input-group input-group-sm">
-        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-navbar" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-      </div>
-    </form>
-
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
       <!-- Messages Dropdown Menu -->
@@ -167,7 +154,7 @@ $arLangMessage = \Bitrix\Main\Localization\Loc::loadLanguageFile(__FILE__, strto
             <i class="flag-icon flag-icon-us mr-2"></i> English
           </a>
           <a href="#" class="dropdown-item">
-            <i class="flag-icon flag-icon-ru mr-2"></i> German
+            <i class="flag-icon flag-icon-ru mr-2"></i> Русский
           </a>
         </div>
       </li>
