78440b1fc66d5ca0504b3952c850b5e988c31e97
Maksim Kubrak <web@maksimkubrak.ru>
2019-08-15 00:13:59 +0300

    add lang templat

---
+++ lang/en/pages.php
@@ -0,0 +1,9 @@
+<?php
+$MESS["NOTIFY_TITLE"] = "Notification";
+$MESS["NOTIFY_NEXTPAGE"] = "Next page";
+$MESS["NOTIFY_HOMEPAGE"] = "Home";
+$MESS["NOTIFY_NEW_MESS"] = "New messages";
+$MESS["NOTIFY_VIEWED_MESS"] = "Viewed messages";
+$MESS["NOTIFY_TABLE_DATE"] = "Date";
+$MESS["NOTIFY_TABLE_DESCRIPTION"] = "Description";
+?>
---
+++ lang/ru/pages.php
@@ -0,0 +1,9 @@
+<?php
+$MESS["NOTIFY_TITLE"] = "Оповещения";
+$MESS["NOTIFY_NEXTPAGE"] = "Следующая страница";
+$MESS["NOTIFY_HOMEPAGE"] = "Главная";
+$MESS["NOTIFY_NEW_MESS"] = "Новые сообщения";
+$MESS["NOTIFY_VIEWED_MESS"] = "Просмотренные сообщения";
+$MESS["NOTIFY_TABLE_DATE"] = "Дата";
+$MESS["NOTIFY_TABLE_DESCRIPTION"] = "Сообщение";
+?>
